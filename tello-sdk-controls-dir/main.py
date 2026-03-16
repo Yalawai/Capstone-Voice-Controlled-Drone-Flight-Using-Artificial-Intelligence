52001dd53708c8c868cc6021e81df5aec35ef1b8
rohanw-code <u3230018@uni.canberra.edu.au>
2026-03-17 00:21:20 +1100

    Set up code to take image from drone camera and save it to jpg.

--- tello-sdk-controls-dir/main.py
+++ tello-sdk-controls-dir/main.py
@@ -1,8 +1,31 @@
 from djitellopy import Tello
+import time
+import cv2
+from numpy.testing.print_coercion_tables import print_coercion_table
 
 tello = Tello()
 tello.connect()
 
-print("Battery:", tello.get_battery())
-print("take off", tello.takeoff())
-print("land", tello.land())
+try:
+    # Drone System Diagnostics
+    print("Drone System Diagnostics")
+    print("Battery:",tello.get_battery())
+    print("Temperature:",tello.get_temperature())
+    print("flight Time:",tello.get_flight_time())
+
+    # Turns on camera
+    tello.streamon()
+    time.sleep(2)
+
+    # Extracts image from video stream
+    frame_read = tello.get_frame_read()
+    frame = frame_read.frame
+
+    # Save image to jpg
+    cv2.imwrite("test.jpg", frame)
+    print("Photo saved to jpg")
+
+finally:
+    # Turns off camera
+    tello.streamoff()
+    tello.end()
