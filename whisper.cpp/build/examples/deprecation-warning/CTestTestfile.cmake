d0ab1b299fdfee989a0b3da1a5e1752e24bb5d31
Kinley Gyeltshen <kinlexgyeltshen1@gmail.com>
2026-03-18 17:06:09 +1100

    Speech to Text Transcription Working using whisper.cpp

---
+++ whisper.cpp/build/examples/deprecation-warning/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kinle/OneDrive/Desktop/Capstone/Capstone-Voice-Controlled-Drone-Flight-Using-Artificial-Intelligence/whisper.cpp/examples/deprecation-warning
+# Build directory: C:/Users/kinle/OneDrive/Desktop/Capstone/Capstone-Voice-Controlled-Drone-Flight-Using-Artificial-Intelligence/whisper.cpp/build/examples/deprecation-warning
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ whisper.cpp/build/examples/vad-speech-segments/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kinle/OneDrive/Desktop/Capstone/Capstone-Voice-Controlled-Drone-Flight-Using-Artificial-Intelligence/whisper.cpp/examples/vad-speech-segments
+# Build directory: C:/Users/kinle/OneDrive/Desktop/Capstone/Capstone-Voice-Controlled-Drone-Flight-Using-Artificial-Intelligence/whisper.cpp/build/examples/vad-speech-segments
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
